a2f177cbc2abc9578fb39e703b2a4627a6c349f8
JinfengChen <jinfeng7chen@gmail.com>
2020-02-08 23:52:04 -0800

    Update work.sh

--- rice_RILs_mPing_scripts/Construction_of_recombination_bin_and_linkage_map/work.sh
+++ rice_RILs_mPing_scripts/Construction_of_recombination_bin_and_linkage_map/work.sh
@@ -1,5 +1,3 @@
-echo "map reads to reference genome"
-perl make_genotype_makefile_gz_slurm.pl -r /rhome/cjinfeng/Rice/RIL/Illumina -o /rhome/cjinfeng/Rice/RIL/genotypes -f . -i genotype_sample_illuminaID.txt
 echo "prepare parental genotype files"
 sbatch step01.parent.sh
 echo "genotype RILs"
